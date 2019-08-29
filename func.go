acf3256e8e6c6813bca3ac0d9e174bfe8f172f96
chenyan <2545205707@qq.com>
2019-08-29 18:01:43 +0800

    add 2 个工具方法

--- func.go
+++ func.go
@@ -34,6 +34,60 @@ func Strtotime(formatTimeStr string) int {
 	return int(formatTime.Unix())
 }
 
+func StripTags(src string) string {
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+	return strings.TrimSpace(src)
+}
+
+//格式化时间
+//Date("Y-m-d H:i:s", ...时间戳)
+func Date(format string, timesteamp ... int64) string {
+	var tt time.Time
+	if len(timesteamp) > 0 && timesteamp[0] > 0 {
+		tt = time.Unix(timesteamp[0], 0)
+	}else {
+		tt = time.Now()
+	}
+
+	var defaultFormat = make(map[string]string, 0)
+	defaultFormat["2006"] = "Y"
+	defaultFormat["01"] = "m"
+	defaultFormat["02"] = "d"
+	defaultFormat["15"] = "H"
+	defaultFormat["04"] = "i"
+	defaultFormat["05"] = "s"
+
+	for key, val := range defaultFormat {
+		format = strings.Replace(format, val, key, -1)
+	}
+	return tt.Format(format)
+}
+
+//获取当前时间
+func CurrentDate() string {
+	var t = time.Now()
+	return Date("Y-m-d H:i:s", t.Unix())
+}
+
+//获取当前时间戳
+func Time() int64 {
+	var t = time.Now()
+	return t.Unix()
+}
 
 func DelAllHtmlTag(src string) string {
 	//将HTML标签全转换成小写
